67fabf7693781be5b94196ad690d122904a282e9
masonnicoletti <mason.f.nicoletti@gmail.com>
2025-12-03 15:07:02 -0500

    Make class updates to app

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 import os
-import boto3
 import json
+import boto3
 from chalice import Chalice
 from botocore.exceptions import ClientError
+
 
 app = Chalice(app_name='s3-events')
 app.debug = True
@@ -14,18 +15,26 @@ DYNAMODB_TABLE_NAME = os.environ.get('DYNAMODB_TABLE_NAME', '')
 
 @app.on_s3_event(bucket=S3_BUCKET, events=['s3:ObjectCreated:*'], suffix='.json')
 def s3_handler(event):
+    # Log event that triggered lambda
     app.log.debug(f"Received bucket event: {event.bucket}, key: {event.key}")
+    # Read and parse file from S3
     data = get_s3_object(event.bucket, event.key)
+    # Insert data into DynamoDB Table
     insert_data_into_dynamodb(data)
     return data
 
+
 def get_s3_object(bucket, key):
+    # get the object from s3
     s3 = boto3.client('s3')
     try:
         response = s3.get_object(Bucket=bucket, Key=key)
-        return json.loads(response['Body'].read().decode('utf-8'))
-    except Exception as e:
-        print(e)
+        message = json.loads(response['Body'].read().decode('utf-8'))
+        return message
+    except ClientError as e:
+        app.log.error(f"Error fetching object from S3: {e}")
+        raise e
+
 
 def insert_data_into_dynamodb(data):
     dynamodb = boto3.resource('dynamodb')
@@ -46,6 +55,7 @@ def insert_data_into_dynamodb(data):
         app.log.error(f"Error inserting data into DynamoDB: {e}")
         raise e
 
+
 @app.route('/access', methods=['GET'])
 def get_access():
     dynamodb = boto3.resource('dynamodb')
@@ -55,5 +65,5 @@ def get_access():
         sorted_items = sorted(items, key=lambda x: x['access_time'])
         return sorted_items
     except ClientError as e:
-        app.log.error(f"Error scanning DynamoDB table: {e}")
+        app.log.error(f"Error scanning DynamoDB table: {e}") 
         raise e
